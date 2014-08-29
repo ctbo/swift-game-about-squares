302a498b4821a573e753b305d9eadb7ac3244ee3
Harald Bögeholz <bo@ct.de>
2014-08-29 13:29:47 +0200

    Initial Commit

---
+++ game-about-squares/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  game-about-squares
+//
+//  Created by Harald Bögeholz on 29.08.14.
+//  Copyright (c) 2014 Harald Bögeholz. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
